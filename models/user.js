8425458aac3001d0cc9c9ab0c04df42a4f06deaf
thread-koder <mohamad.fberjawi@gmail.com>
2021-10-25 18:00:51 +0300

    Create User database model

---
+++ models/user.js
@@ -0,0 +1,54 @@
+import mongoose from "mongoose";
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema(
+  {
+    username: {
+      // login
+      type: String,
+      required: true,
+    },
+    github_id: {
+      type: String,
+      required: true,
+    },
+    avatar_url: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+    },
+    location: {
+      type: String,
+    },
+    bio: {
+      type: String,
+    },
+    company: {
+      type: String,
+    },
+    isHireable: {
+      type: Boolean,
+    },
+    github_profile_url: {
+      // html_url
+      type: String,
+      required: true,
+    },
+    user_createdAt: {
+      type: String,
+    },
+    commit_contributions: {
+      type: Array,
+    },
+    score: {
+      type: Number,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
