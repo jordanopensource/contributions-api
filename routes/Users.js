a808b2d8fdff8d681899766e9e293abca96f5f5f
thread-koder <mohamad.fberjawi@gmail.com>
2021-10-25 19:41:02 +0300

    Create Users route to get all users with sorting

--- routes/Users.js
+++ routes/Users.js
@@ -1,11 +1,28 @@
-import { Mongoose } from "mongoose";
+import Mongoose from "mongoose";
 import express from "express";
+
+import User from "../models/user.js";
 
 const router = express.Router();
 
-router.get("/users", (req, res) => {
+router.get("/users", async (req, res) => {
+  let users = [];
+  let { sort, limit } = req.query;
+  limit = limit ? Number(limit) : 100;
+  switch (sort) {
+    case "asc":
+      users = await User.find().sort({ score: 1 }).limit(limit);
+      break;
+    case "desc":
+      users = await User.find().sort({ score: -1 }).limit(limit);
+      break;
+    default:
+      users = await User.find().sort({}).limit(limit);
+      break;
+  }
   res.status(200).json({
     success: true,
+    data: users,
   });
 });
 
