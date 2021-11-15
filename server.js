dbb0cb1cffe0bdd21f9dd6168cfa3b9530bc48c1
thread-koder <mohamad.fberjawi@gmail.com>
2021-11-15 18:44:29 +0200

    Disable the docs on production builds

--- server.js
+++ server.js
@@ -18,21 +18,6 @@ dotenv.config({
   path: "./config.env",
 });
 
-const options = {
-  definition: {
-    openapi: "3.0.0",
-    info: {
-      title: "JOSA Top Contributors API",
-      version: "1.0.0",
-      description: "Express based APIs for the JOSA Top Contributors.",
-    },
-    servers: [{ url: `http://${process.env.HOST}:${process.env.PORT}` }],
-  },
-  apis: ["./routes/*.js"],
-};
-
-const swaggerSpecs = swaggerJsDoc(options);
-
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -44,6 +29,21 @@ const port = process.env.PORT;
 if (process.env.NODE_ENV !== "production") {
   app.use(morgan("dev"));
   app.use(responseTime());
+
+  const options = {
+    definition: {
+      openapi: "3.0.0",
+      info: {
+        title: "JOSA Top Contributors API",
+        version: "1.0.0",
+        description: "Express based APIs for the JOSA Top Contributors.",
+      },
+      servers: [{ url: `http://${process.env.HOST}:${process.env.PORT}` }],
+    },
+    apis: ["./routes/*.js"],
+  };
+  const swaggerSpecs = swaggerJsDoc(options);
+  app.use("/api/v1/docs", swaggerUI.serve, swaggerUI.setup(swaggerSpecs));
 }
 
 const ConnectToDB = async () => {
@@ -53,10 +53,9 @@ const ConnectToDB = async () => {
   });
   console.log("Connected to the database");
 };
-app.use("/api/v1/docs", swaggerUI.serve, swaggerUI.setup(swaggerSpecs));
-app.use("/api/v1", usersRoute);
-app.use("/api/v1", organizationsRoute);
-app.use("/api/v1", contributionsRoute);
+app.use("/v1", usersRoute);
+app.use("/v1", organizationsRoute);
+app.use("/v1", contributionsRoute);
 
 app.get("*", (req, res) => {
   res.status(404).json({
