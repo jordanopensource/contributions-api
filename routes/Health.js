5b29946d24c4e8bf385235a40970f9a9aab97073
thread-koder <mohamad.fberjawi@gmail.com>
2022-04-21 18:05:48 +0300

    Document the Health.js code

--- routes/Health.js
+++ routes/Health.js
@@ -2,6 +2,7 @@ import express from "express";
 
 const router = express.Router();
 
+/* This is a route that will be used to check if the server is up and running. */
 router.get("/healthcheck", async (req, res) => {
   res.status(200).json({
     message: "I'm alive, freedom!!!!",
