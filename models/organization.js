bc793facc6d6a88efb800de08310b56d1f225812
thread-koder <mohamad.fberjawi@gmail.com>
2022-04-21 16:57:53 +0300

    Document the organization.js code

--- models/organization.js
+++ models/organization.js
@@ -2,38 +2,46 @@ import mongoose from "mongoose";
 import mongoosePaginate from "mongoose-paginate-v2";
 const Schema = mongoose.Schema;
 
+/* Creating a schema for the organization model. */
 const organizationSchema = new Schema(
   {
+    /* The login/username of the organization */
     username: {
-      // login
       type: String,
       required: true,
     },
+    /* The id of the organization on github. */
     github_id: {
       type: String,
       required: true,
     },
+    /* The url of the organization's profile picture. */
     avatar_url: {
       type: String,
       required: true,
     },
+    /* The name of the organization */
     name: {
       type: String,
     },
+    /* The location of the organization */
     location: {
       type: String,
     },
+    /* The url of the organization's profile on github. */
     github_profile_url: {
-      // html_url
       type: String,
       required: true,
     },
+    /* The date the organization was created. */
     organization_createdAt: {
       type: String,
     },
+    /* An array of the repositories of the organization. */
     repositories: {
       type: Array,
     },
+    /* An array of the members of the organization. */
     members: {
       type: Array,
     },
@@ -42,7 +50,9 @@ const organizationSchema = new Schema(
   { timestamps: true }
 );
 
+/* A plugin that allows us to paginate the data. */
 organizationSchema.plugin(mongoosePaginate);
+/* Creating a model for the organization schema. */
 const Organization = mongoose.model("Organization", organizationSchema);
 
 export default Organization;
