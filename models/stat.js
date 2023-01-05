d4f22dfead9b6768bd5f4f5d1a20a011edea9a2e
thread-koder <mohamad.fberjawi@gmail.com>
2023-01-05 14:29:49 +0300

    Add the stat model

---
+++ models/stat.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+const Schema = mongoose.Schema;
+
+const statSchema = new Schema(
+  {
+    total_users: {
+      type: Number,
+      required: true,
+    },
+    total_orgs: {
+      type: Number,
+      required: true,
+    },
+    total_commits: {
+      type: Number,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Stat = mongoose.model("Stat", statSchema);
+
+export default Stat;
